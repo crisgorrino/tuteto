179f23e462dffe549994cac7616a1b97992b14db
crisgorrino <cgorrino@gmail.com>
2014-04-29 11:16:14 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="css/normalize.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script src="js/js.js" type="text/javascript"></script>
+<title>Bienvenido a Tu Teto</title>
+</head>
+<body>
+<!--MAST-->	
+	<article class="container home-bg">
+		<section class="inner cf">
+			<img src="img/tuteto_logo_lrg.png" alt="Tu tueto" class="left">
+			<div class="home-mast-info right text-right">
+				<h1>&iexcl;TE HACEMOS TU TAREA!</h1>
+				<p>Ensayos, resumenes, ppt, AutoCAD, contadur&iacute;a, finanzas, investigaciones, etc.<br> 
+					&iexcl;Tu Teto puede con todo!</p>				
+				<form class="home-search-form cf">
+					<input type="search" placeholder="Comienza escribiendo la materia/ &aacute;rea de tu tarea">
+					<input type="submit" class="blue_btn" value="Buscar">					
+				</form>			
+			</div>
+		</section>	
+	</article>
+<!--MAST-->	
+
+<!--register login form-->
+<article class="container gray-bg">
+	<section class="inner cf">
+		<div class="left home-bar">
+			<p class="yellow inline">&iquest;Te pagar&aacute;n por hacer tareas!, reg&iacute;strate gratis</p>
+			<a href="#" class="red_btn">Reg&iacute;strate</a>
+			<a href="#" class="blue_btn">Carga tu Comprobante</a>
+		</div>
+		
+		<div class="right home-bar">
+			<form class="home-login">
+				<input type="text" placeholder="Usuario">
+				<input type="password" placeholder="Contrase&ntilde;a">
+				<input type="submit" value="Login" class="blue_btn">
+				<a href="#" class="olvido-contra">&iquest;Olvido su contrase&ntilde;a?</a>
+			</form>
+		</div>
+	</section>
+</article>	
+<!--register login form-->
+
+<!--acerca de nosotros-->
+<article class="container">
+	<section class="inner cf">
+		<h2>Acerca de nosotros</h2>
+		<div class="col-2 left">
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+
+				<p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
+		</div>
+		
+		<div class="col-2 right">
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+
+				<p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
+
+		</div>
+	</section>
+</article>
+<!--acerca de nosotros-->
+
+<footer class="gray-bg ">
+	<section class="inner-padded cf">
+		<div class="footer-info left">
+			<a href="" class="icon-facebook"></a>
+			<a href="" class="icon-twitter"></a>
+			<a href="mailto:ventas@tuteto.mx" class="ventas-mail">ventas@tuteto.mx</a>
+		</div>
+		
+		<div class="footer-info right text-right">
+			<p>Consulta</p>
+			<a href="" class="ventas-mail">Aviso de privacidad</a> 
+			<a href="" class="ventas-mail">Terminos y condiciones</a> 
+		</div>
+			
+	</section>
+</footer>	
+	
+</body>
+</html> 
---
+++ tetos.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="css/normalize.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script src="js/js.js" type="text/javascript"></script>
+<title>Listado de Tetos</title>
+</head>
+<body>
+
+<!--header-->
+<header>
+	<section class="inner cf">
+		<a href="" class="logo left"><img src="img/tuteto_logo.png" alt="tu teto"></a>
+		<div class="top-info cf">
+			<p><span class="yellow">Resultados para:</span> Matem&aacute;ticas</p>
+			
+			<form class="top-search-form">
+				<input type="search" placeholder="Buscar materia">
+				<a href="" class="icon-search top-search-sub"></a>
+			</form>
+		</div>
+	</section>
+</header>
+<!--header-->
+
+<!--filtros-->
+<article class="container">
+	<section class="inner-padded">
+		<p class="inline">Filtrar por:</p>
+		
+		<div class="styled-select inline">
+		   <select>
+		      <option>Ranking</option>
+		      <option>juan</option>
+		   </select>
+		</div>
+		
+	</section>
+</article>
+<!--filtros-->
+
+<footer class="gray-bg ">
+	<section class="inner-padded cf">
+		<div class="footer-info left">
+			<a href="" class="icon-facebook"></a>
+			<a href="" class="icon-twitter"></a>
+			<a href="mailto:ventas@tuteto.mx" class="ventas-mail">ventas@tuteto.mx</a>
+		</div>
+		
+		<div class="footer-info right text-right">
+			<p>Consulta</p>
+			<a href="" class="ventas-mail">Aviso de privacidad</a> 
+			<a href="" class="ventas-mail">Terminos y condiciones</a> 
+		</div>
+			
+	</section>
+</footer>	
+	
+</body>
+</html> 
